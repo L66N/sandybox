27fa5f6c13ca77c37760490343bb0587580fb42f
lana <fufarova.svetlana@gmail.com>
2025-05-19 03:08:46 -0700

    update recommendation system

--- Recommendation_machine.py
+++ Recommendation_machine.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import difflib
-from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 movies_data = pd.read_csv('movies.csv')
